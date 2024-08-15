dbfe2015476418d7896a049662c2548be0064dff
zestbear <zestbear90@gmail.com>
2024-08-15 22:04:44 +0900

    initial commit

---
+++ src/main/java/com/server/trading/auto/TradingServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.trading.auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/trading/auto/TradingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.trading.auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
