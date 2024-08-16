a0d07a81356c1bf52e5836eec975b50765bf3ba1
zestbear <zestbear90@gmail.com>
2024-08-16 20:42:01 +0900

    fix: fix int to Integer

--- src/main/java/com/server/trading/auto/domain/Order.java
+++ src/main/java/com/server/trading/auto/domain/Order.java
@@ -14,19 +14,19 @@ import lombok.NoArgsConstructor;
 public class Order {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String uuid;             // 주문의 고유 아이디
-    private String side;             // 주문 종류
-    private String ord_type;         // 주문 방식
-    private String price;            // 주문 당시 화폐 가격
-    private String state;            // 주문 상태
-    private String market;           // 마켓의 유일키
-    private String created_at;       // 주문 생성 시간
-    private String volume;           // 사용자가 입력한 주문 양
-    private String remaining_volume; // 체결 후 남은 주문 양
-    private String reserved_fee;     // 수수료로 예약된 비용
-    private String remaining_fee;    // 남은 수수료
-    private String paid_fee;         // 사용된 수수료
-    private String locked;           // 거래에 사용중인 비용
-    private String executed_volume;  // 체결된 양
-    private int trades_count;        // 해당 주문에 걸린 체결 수
+    private String uuid;                // 주문의 고유 아이디
+    private String side;                // 주문 종류
+    private String ordType;             // 주문 방식
+    private String price;               // 주문 당시 화폐 가격
+    private String state;               // 주문 상태
+    private String market;              // 마켓의 유일키
+    private String createdAt;           // 주문 생성 시간
+    private String volume;              // 사용자가 입력한 주문 양
+    private String remainingVolume;     // 체결 후 남은 주문 양
+    private String reservedFee;         // 수수료로 예약된 비용
+    private String remainingFee;        // 남은 수수료
+    private String paidFee;             // 사용된 수수료
+    private String locked;              // 거래에 사용중인 비용
+    private String executedVolume;      // 체결된 양
+    private Integer tradesCount;        // 해당 주문에 걸린 체결 수
 }
