ca40ca6e37b0bfab1eb250996b4456fe66b3b7a6
zestbear <zestbear90@gmail.com>
2024-08-20 22:38:52 +0900

    feature: add error handling

--- src/main/java/com/server/trading/auto/util/api/ExceptionCode.java
+++ src/main/java/com/server/trading/auto/util/api/ExceptionCode.java
@@ -11,7 +11,8 @@ public enum ExceptionCode {
     MINUTE_CANDLE_FAILURE(4003, "분봉 캔들 정보를 가져오는 작업에 실패했습니다."),
     DELETE_ORDER_FAILURE(4004, "주문을 취소하는 작업에 실패했습니다."),
     GET_ORDER_FAILURE(4005, "주문 목록을 가져오는 작업에 실패했습니다."),
-    POST_ORDER_FAILURE(4006, "주문에 실패했습니다.");
+    POST_ORDER_FAILURE(4006, "주문에 실패했습니다."),
+    ORDER_NOT_EXIST(4007, "해당 코인을 보유하고 있지 않습니다.");
 
     private int code;
     private String message;
