d4f20edb8748137e7413854a324de8dd688eaa26
zestbear <zestbear90@gmail.com>
2024-08-25 02:16:44 +0900

    feature: add columns to match with api response

--- src/main/java/com/server/trading/auto/domain/Value.java
+++ src/main/java/com/server/trading/auto/domain/Value.java
@@ -12,22 +12,30 @@ import lombok.*;
 public class Value {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String market;              // 종목 구분 코드
-    private String trade_date;          // 최근 거래 일자(UTC)
-    private String trade_time;          // 최근 거래 시각(UTC)
-    private String trade_date_kst;      // 최근 거래 일자(KST)
-    private String trade_time_kst;      // 최근 거래 시각(KST)
-    private Long trade_timestamp;       // 최근 거래 일시(UTC)
-    private Double opening_price;       // 시가
-    private Double high_price;          // 고가
-    private Double low_price;           // 저가
-    private Double trade_price;         // 종가(현재가)
-    private Double prev_closing_price;  // 전일 종가(UTC 0시 기준)
-    private String change;              // 보합/상승/하락
-    private Double change_price;        // 변화액의 절대값
-    private Double change_rate;         // 변화율의 절대값
-    private Double signed_change_price; // 부호가 있는 변화액
-    private Double signed_change_rate;  // 부호가 있는 변화율
-    private Double trade_volume;        // 가장 최근 거래량
-    private Long timestamp;             // 타임스탬프
+    private String market;                  // 종목 구분 코드
+    private String trade_date;              // 최근 거래 일자(UTC)
+    private String trade_time;              // 최근 거래 시각(UTC)
+    private String trade_date_kst;          // 최근 거래 일자(KST)
+    private String trade_time_kst;          // 최근 거래 시각(KST)
+    private Long trade_timestamp;           // 최근 거래 일시(UTC)
+    private Double opening_price;           // 시가
+    private Integer high_price;             // 고가
+    private Integer low_price;              // 저가
+    private Integer trade_price;            // 종가(현재가)
+    private Double prev_closing_price;      // 전일 종가(UTC 0시 기준)
+    private String change;                  // 보합/상승/하락
+    private Double change_price;            // 변화액의 절대값
+    private Double change_rate;             // 변화율의 절대값
+    private Double signed_change_price;     // 부호가 있는 변화액
+    private Double signed_change_rate;      // 부호가 있는 변화율
+    private Double trade_volume;            // 가장 최근 거래량
+    private Double acc_trade_price;         // 누적 거래대금(UTC 0시 기준)
+    private Double acc_trade_price_24h;     // 24시간 누적 거래대금
+    private Double acc_trade_volume;        // 누적 거래량(UTC 0시 기준)
+    private Double acc_trade_volume_24h;    // 24시간 누적 거래량
+    private Double highest_52_week_price;   // 52주 신고가
+    private String highest_52_week_date;    // 52주 신고가 달성일
+    private Double lowest_52_week_price;    // 52주 신저가
+    private String lowest_52_week_date;     // 52주 신저가 달성일
+    private Long timestamp;                 // 타임스탬프
 }
--- src/main/java/com/server/trading/auto/dto/ValueSendRequestDto.java
+++ src/main/java/com/server/trading/auto/dto/ValueSendRequestDto.java
@@ -1,9 +1,11 @@
 package com.server.trading.auto.dto;
 
+import com.google.gson.JsonObject;
 import com.server.trading.auto.domain.Value;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
@@ -15,9 +17,9 @@ public class ValueSendRequestDto {
     private String trade_time_kst;
     private Long trade_timestamp;
     private Double opening_price;
-    private Double high_price;
-    private Double low_price;
-    private Double trade_price;
+    private Integer high_price;
+    private Integer low_price;
+    private Integer trade_price;
     private Double prev_closing_price;
     private String change;
     private Double change_price;
@@ -25,7 +27,17 @@ public class ValueSendRequestDto {
     private Double signed_change_price;
     private Double signed_change_rate;
     private Double trade_volume;
+    private Double acc_trade_price;
+    private Double acc_trade_price_24h;
+    private Double acc_trade_volume;
+    private Double acc_trade_volume_24h;
+    private Double highest_52_week_price;
+    private String highest_52_week_date;
+    private Double lowest_52_week_price;
+    private String lowest_52_week_date;
     private Long timestamp;
+    @Setter
+    private Double rsi;
 
     @Builder
     public ValueSendRequestDto(
@@ -36,9 +48,9 @@ public class ValueSendRequestDto {
             String trade_time_kst,
             Long trade_timestamp,
             Double opening_price,
-            Double high_price,
-            Double low_price,
-            Double trade_price,
+            Integer high_price,
+            Integer low_price,
+            Integer trade_price,
             Double prev_closing_price,
             String change,
             Double change_price,
@@ -46,11 +58,19 @@ public class ValueSendRequestDto {
             Double signed_change_price,
             Double signed_change_rate,
             Double trade_volume,
+            Double acc_trade_price,
+            Double acc_trade_price_24h,
+            Double acc_trade_volume,
+            Double acc_trade_volume_24h,
+            Double highest_52_week_price,
+            String highest_52_week_date,
+            Double lowest_52_week_price,
+            String lowest_52_week_date,
             Long timestamp
     ) {
         this.market = market;
         this.trade_date = trade_date;
-        this.trade_time=trade_time;
+        this.trade_time = trade_time;
         this.trade_date_kst = trade_date_kst;
         this.trade_time_kst = trade_time_kst;
         this.trade_timestamp = trade_timestamp;
@@ -65,8 +85,46 @@ public class ValueSendRequestDto {
         this.signed_change_price = signed_change_price;
         this.signed_change_rate = signed_change_rate;
         this.trade_volume = trade_volume;
+        this.acc_trade_price = acc_trade_price;
+        this.acc_trade_price_24h = acc_trade_price_24h;
+        this.acc_trade_volume = acc_trade_volume;
+        this.acc_trade_volume_24h = acc_trade_volume_24h;
+        this.highest_52_week_price = highest_52_week_price;
+        this.highest_52_week_date = highest_52_week_date;
+        this.lowest_52_week_price = lowest_52_week_price;
+        this.lowest_52_week_date = lowest_52_week_date;
         this.timestamp = timestamp;
     }
+
+    public ValueSendRequestDto(JsonObject object) {
+        this.market = object.get("market").getAsString();
+        this.trade_date = object.get("trade_date").getAsString();
+        this.trade_time = object.get("trade_time").getAsString();
+        this.trade_date_kst = object.get("trade_date_kst").getAsString();
+        this.trade_time_kst = object.get("trade_time_kst").getAsString();
+        this.trade_timestamp = object.get("trade_timestamp").getAsLong();
+        this.opening_price = object.get("opening_price").getAsDouble();
+        this.high_price = object.get("high_price").getAsInt();
+        this.low_price = object.get("low_price").getAsInt();
+        this.trade_price = object.get("trade_price").getAsInt();
+        this.prev_closing_price = object.get("prev_closing_price").getAsDouble();
+        this.change = object.get("change").getAsString();
+        this.change_price = object.get("change_price").getAsDouble();
+        this.change_rate = object.get("change_rate").getAsDouble();
+        this.signed_change_price = object.get("signed_change_price").getAsDouble();
+        this.signed_change_rate = object.get("signed_change_rate").getAsDouble();
+        this.trade_volume = object.get("trade_volume").getAsDouble();
+        this.acc_trade_price = object.get("acc_trade_price").getAsDouble();
+        this.acc_trade_price_24h = object.get("acc_trade_price_24h").getAsDouble();
+        this.acc_trade_volume = object.get("acc_trade_volume").getAsDouble();
+        this.acc_trade_volume_24h = object.get("acc_trade_volume_24h").getAsDouble();
+        this.highest_52_week_price = object.get("highest_52_week_price").getAsDouble();
+        this.highest_52_week_date = object.get("highest_52_week_date").getAsString();
+        this.lowest_52_week_price = object.get("lowest_52_week_price").getAsDouble();
+        this.lowest_52_week_date = object.get("lowest_52_week_date").getAsString();
+        this.timestamp = object.get("timestamp").getAsLong();
+    }
+
 
     public Value toEntity() {
         return Value.builder()
@@ -87,6 +145,14 @@ public class ValueSendRequestDto {
                 .signed_change_price(signed_change_price)
                 .signed_change_rate(signed_change_rate)
                 .trade_volume(trade_volume)
+                .acc_trade_price(acc_trade_price)
+                .acc_trade_price_24h(acc_trade_price_24h)
+                .acc_trade_volume(acc_trade_volume)
+                .acc_trade_volume_24h(acc_trade_volume_24h)
+                .highest_52_week_date(highest_52_week_date)
+                .highest_52_week_price(highest_52_week_price)
+                .lowest_52_week_date(lowest_52_week_date)
+                .lowest_52_week_price(lowest_52_week_price)
                 .timestamp(timestamp)
                 .build();
     }
