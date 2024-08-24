bca92c8c4c20154c6cc285e292e7d82fd72fb499
zestbear <zestbear90@gmail.com>
2024-08-25 02:17:10 +0900

    feature: separate value/order service

--- src/main/java/com/server/trading/auto/service/OrderService.java
+++ src/main/java/com/server/trading/auto/service/OrderService.java
@@ -4,6 +4,9 @@ import com.server.trading.auto.domain.Order;
 import com.server.trading.auto.domain.OrderRepository;
 import com.server.trading.auto.dto.OrderCreateRequestDto;
 import com.server.trading.auto.dto.OrderResponseDto;
+import com.server.trading.auto.service.api.order.DeleteOrder;
+import com.server.trading.auto.service.api.order.GetOrders;
+import com.server.trading.auto.service.api.order.PostOrders;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +16,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private final GetOrders getOrders;
+    private final PostOrders postOrders;
+    private final DeleteOrder deleteOrder;
 
     @Transactional
     public OrderResponseDto save(OrderCreateRequestDto orderCreateRequestDto) {
