8101c76e60d58592e5069c5422f927a1cd62f626
zestbear <zestbear90@gmail.com>
2024-08-26 23:42:20 +0900

    feature: add scheduling (every 30sec)

--- src/main/java/com/server/trading/auto/TradingServerApplication.java
+++ src/main/java/com/server/trading/auto/TradingServerApplication.java
@@ -1,10 +1,53 @@
 package com.server.trading.auto;
 
+import com.server.trading.auto.service.OrderService;
+import com.server.trading.auto.service.ValueService;
+import com.server.trading.auto.service.api.UpbitBucket;
+import com.server.trading.auto.service.api.account.GetAccounts;
+import com.server.trading.auto.service.api.candle.CurrentValue;
+import com.server.trading.auto.service.api.candle.MinuteCandle;
+import com.server.trading.auto.service.api.order.DeleteOrder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@Slf4j
+@RequiredArgsConstructor
+@EnableScheduling
 @SpringBootApplication
 public class TradingServerApplication {
+
+	private final OrderService orderService;
+	private final ValueService valueService;
+
+	private final DeleteOrder deleteOrder;
+	private final GetAccounts getAccounts;
+	private final CurrentValue currentValue;
+	private final MinuteCandle minuteCandle;
+
+	@Scheduled(fixedDelay = 30000)
+	public void execute() {
+		// get data from api
+		UpbitBucket upbitBucket = new UpbitBucket();
+		upbitBucket
+				.add(currentValue)
+				.add(minuteCandle)
+				.add(getAccounts);
+		upbitBucket.execute();
+		// delete all orders not executed
+		deleteOrder.deleteOrder();
+		// trade
+		orderService.order();
+		// send data to batch server
+		valueService.send();
+		// clear data
+		upbitBucket.clear();
+
+		log.info("===== EXECUTED =====");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradingServerApplication.class, args);
